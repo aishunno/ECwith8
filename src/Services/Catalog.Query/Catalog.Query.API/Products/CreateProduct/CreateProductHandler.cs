60a53e63f198b1e64865aa92dfac30b58cf61de3
arifulIslamSelise <ariful.islam@selise.ch>
2024-03-19 23:13:01 +0600

    Abstraction added for IRequest

---
+++ src/Services/Catalog.Query/Catalog.Query.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,21 @@
+﻿using BuildingBlocks.CQRS;
+using MediatR;
+
+namespace Catalog.Query.API.Products.CreateProduct;
+
+public record CreateProductCommand(
+    string Name, 
+    string Description,
+    List<string> Category,
+    string ImageFile,
+    decimal Price) : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
